37dee0c65c077797b4728054c82f961694fe22e3
MCMike <quinterovillegasmm@gmail.com>
2020-10-26 18:02:55 -0600

    Added Sun

--- Window.cs
+++ Window.cs
@@ -7,24 +7,84 @@ namespace MiniProyecto1 {
 
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings,nativeWindowSettings){}
 
+        private readonly float[] _vertices =
+        {
+            // Position         Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
+        private readonly uint[] _indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+        private int ebo;
+        private int vbo;
+        private int vao;
+        private Shader shader;
+        private Texture texture;
+
         protected override void OnLoad()
         {
-            GL.ClearColor(1.0f,1.0f,1.0f,1.0f);
+            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+            vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("C:\\Users\\maqui\\Documents\\MiniProyecto1\\Shaders\\vert.glsl", "C:\\Users\\maqui\\Documents\\MiniProyecto1\\Shaders\\frag.glsl");
+            shader.Use();
+
+            texture = new Texture("C:\\Users\\maqui\\Documents\\MiniProyecto1\\Texturas\\sol.jpg");
+            texture.Use();
+
+            vao = GL.GenVertexArray();
+            GL.BindVertexArray(vao);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+
+
+            var vertexLocation = shader.GetAttribLocation("aPosition");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+            var texCoordLocation = shader.GetAttribLocation("aTexCoord");
+            GL.EnableVertexAttribArray(texCoordLocation);
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
             base.OnLoad();
         }
-        protected override void OnRenderFrame(FrameEventArgs args)
+        protected override void OnRenderFrame(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.BindVertexArray(vao);
+            texture.Use();
+            shader.Use();
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
             SwapBuffers();
-            base.OnRenderFrame(args);
+            base.OnRenderFrame(e);
         }
-        protected override void OnResize(ResizeEventArgs e)
+         protected override void OnResize(ResizeEventArgs e)
         {
             GL.Viewport(0, 0, Size.X, Size.Y);
             base.OnResize(e);
         }
         protected override void OnUnload()
         {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+            GL.DeleteBuffer(vbo);
+            GL.DeleteVertexArray(vao);
+            GL.DeleteProgram(shader.Handle);
+            GL.DeleteTexture(texture.Handle);
             base.OnUnload();
         }
     }
